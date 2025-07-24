0c98b4ab65ac0a05294d7dc6b2119a3845efbf27
Raine Brookshire <112977778+Rxbrooks15@users.noreply.github.com>
2025-07-24 18:34:38 -0500

    Update origami_app.py

--- origami_app.py
+++ origami_app.py
@@ -149,7 +149,7 @@ def process_and_plot(df, highlight_name=None):
     📃<b>Description:</b> %{customdata[5]}<br>
     <extra></extra>
     """,
-    marker=dict(size=9, opacity=0.8),
+    marker=dict(size=6, opacity=1.2),
     hoverlabel=dict(bgcolor="white", font_size=13, font_family="Arial")
 )
     fig.add_trace(go.Scatter(x=X_full.flatten(), y=y_pred, mode='lines', name='Fit', line=dict(color='black')))
@@ -164,8 +164,13 @@ def process_and_plot(df, highlight_name=None):
                 name='🟢 Highlighted',
                 text=match["Name"],
                 textposition="top center",
-                marker=dict(color='green', size=14, symbol='circle-open-dot')
-            ))
+                marker=dict(
+        color='green',
+        size=18,
+        symbol='circle-open-dot',
+        line=dict(width=3, color='darkgreen')  # Bold outline
+    )
+))
 
     st.plotly_chart(fig, use_container_width=True)
     st.markdown(f"### Total Observations: {df.shape[0]-1}")
