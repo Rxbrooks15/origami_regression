c231a4a0776255eca8f395157dea6717ee3487cf
Raine Brookshire <112977778+Rxbrooks15@users.noreply.github.com>
2025-08-01 22:20:56 -0500

    Update origami_app.py

--- origami_app.py
+++ origami_app.py
@@ -219,7 +219,28 @@ def process_and_plot(df, highlight_name=None):
             ))
     
     st.plotly_chart(fig, use_container_width=True)
-    # --- Load dataset from GitHub ---
+   
+st.markdown("""
+The goal of this logarithm regression model is to guide users in selecting origami designs that match their skill level, while also offering an easy way to browse a wide variety of models along with their estimated difficulty scores.
+This regression calculates a **Complexity Score** based on a prior 5-point difficulty rating scale for each model and by analyzing each model's description using **topic modeling** (via Non-negative Matrix Factorization). The technique extracts dominant themes from model descriptions and weighs them to estimate model difficulty
+    
+**Note:** All origami model information and images are sourced from [origami-database.com](https://origami-database.com/models/). The models were not created by me. For inquiries in regard to information the Origami Database please contact the site author directly at **info@origami-database.com**.
+""", unsafe_allow_html=True)
+st.markdown(f"### Total Observations: {df.shape[0]}")
+
+
+# --- Streamlit UI ---
+st.title("📐 Origami Model Complexity Tracker")
+st.markdown("""
+Origami is the traditional Japanese art of paper folding, where a single sheet of paper is transformed into intricate sculptures without cutting or gluing. 
+
+This dashboard provides a collection of origami models and attributes a difficulty/ complexity score to each model. The logarithm regression aims to helps users explore a wide range of origami models with estimated difficulty scores. 
+
+[📁 Check out the Origami Database(https://origami-database.com/models/)
+""", unsafe_allow_html=True)
+
+
+ # --- Load dataset from GitHub ---
 @st.cache_data
 def load_data():
     url = "https://raw.githubusercontent.com/Rxbrooks15/origami_regression/refs/heads/main/Combined_CNN_BERT.csv"
@@ -290,24 +311,6 @@ fig.add_trace(go.Scatter(x=x_range.flatten(), y=y_rf, mode="lines",
 st.plotly_chart(fig, use_container_width=True)
 
 
-st.markdown("""
-The goal of this logarithm regression model is to guide users in selecting origami designs that match their skill level, while also offering an easy way to browse a wide variety of models along with their estimated difficulty scores.
-This regression calculates a **Complexity Score** based on a prior 5-point difficulty rating scale for each model and by analyzing each model's description using **topic modeling** (via Non-negative Matrix Factorization). The technique extracts dominant themes from model descriptions and weighs them to estimate model difficulty
-    
-**Note:** All origami model information and images are sourced from [origami-database.com](https://origami-database.com/models/). The models were not created by me. For inquiries in regard to information the Origami Database please contact the site author directly at **info@origami-database.com**.
-""", unsafe_allow_html=True)
-st.markdown(f"### Total Observations: {df.shape[0]}")
-
-
-# --- Streamlit UI ---
-st.title("📐 Origami Model Complexity Tracker")
-st.markdown("""
-Origami is the traditional Japanese art of paper folding, where a single sheet of paper is transformed into intricate sculptures without cutting or gluing. 
-
-This dashboard provides a collection of origami models and attributes a difficulty/ complexity score to each model. The logarithm regression aims to helps users explore a wide range of origami models with estimated difficulty scores. 
-
-[📁 Check out the Origami Database(https://origami-database.com/models/)
-""", unsafe_allow_html=True)
 # Load CSV
 df = pd.read_csv(CSV_PATH)
 
@@ -410,6 +413,7 @@ st.markdown("""
 """)
 st.image("BERT_regression.png", caption="Folding Time vs Predicted Complexity with Log Regression", use_container_width=True)
 st.image("confusion.png", caption="Confusion Matrix for Classification =0.539", use_container_width=True)
+
 
 
 
